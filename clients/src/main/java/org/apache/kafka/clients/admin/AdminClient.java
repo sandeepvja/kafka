db2d9761d5098a48428f26e1fd6b1323bc747f2d
sandeep.mellacheruvu <sandeep.mellacheruvu@oracle.com>
2019-06-18 22:04:43 +0530

    Removing unused imports

--- clients/src/main/java/org/apache/kafka/clients/admin/AdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/AdminClient.java
@@ -19,7 +19,6 @@ package org.apache.kafka.clients.admin;
 
 import java.time.Duration;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
